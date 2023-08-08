a039cdd09906d1e828b493d43579c2c2b980d1bd
andrewzw <yapzhewei2002@gmail.com>
2023-08-09 00:43:45 +1000

    Added file structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './Assets/logo.svg';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Components/Layout/Layout";
+import About from "./Pages/About/About";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import FAQ from "./Pages/FAQ/FAQ";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Market from "./Pages/Market/Market";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
