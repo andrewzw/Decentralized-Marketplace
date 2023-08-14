1ae09e74997e8f3d5123f7445daba7f0a32350e5
andrewzw <yapzhewei2002@gmail.com>
2023-08-15 09:54:04 +1000

    added grid layout for market page

--- src/Pages/Market/Market.js
+++ src/Pages/Market/Market.js
@@ -6,55 +6,111 @@ import Grid from '@mui/material/Grid';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
+    ...theme.typography.body1,
     padding: theme.spacing(1),
     textAlign: 'center',
     color: theme.palette.text.secondary,
 }));
-const Home = () => {
+
+
+const Market = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Market</h1>
             <Grid container spacing={1} rowSpacing={1} >
-                <Grid item xs={3} md={8}>
-                    <Item>xs=3</Item>
+                <Grid item xs={8}>
+                    <Item>
+                        <h2>Section 1</h2>
+                        <Grid container spacing={1} rowSpacing={1} >
+
+                            <Grid item xs={3}>
+                                <Item>Featured Item 1</Item>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Item>Featured Item 2</Item>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Item>Featured Item 3</Item>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Item>Featured Item 4</Item>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={12}>
+
+                            <h2>Section 2</h2>
+
+                            <Grid container spacing={1} rowSpacing={1} >
+                                <Grid item xs={12}>
+                                    <Item>Tabs</Item>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Item>Listed 1</Item>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Item>Listed 2</Item>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Item>Listed 3</Item>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Item>Listed 4</Item>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Item>Listed 5</Item>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Item>Listed 6</Item>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Item>Listed 7</Item>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Item>Listed 8</Item>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Item>Listed 9</Item>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Item>Listed 10</Item>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Item>Listed 11</Item>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Item>Listed 12</Item>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Item>
                 </Grid>
-                <Grid item xs={3}>
-                    <Item>xs=3</Item>
+
+                <Grid item xs={4}>
+                    <Item>
+                        <h2>Section 1</h2>
+                        <Grid container spacing={1} rowSpacing={1} >
+
+                            <Grid item xs={12}>
+                                <Item>
+                                    <p> You are buying: </p>
+                                    Item Details Section
+                                </Item>
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <Item>
+                                    <p> You are paying: </p>
+                                    Buy Section
+                                </Item>
+                            </Grid>
+                        </Grid>
+
+                    </Item>
                 </Grid>
-                <Grid item xs={3}>
-                    <Item>xs=3</Item>
-                </Grid>
-                <Grid item xs={3}>
-                    <Item>xs=3</Item>
-                </Grid>
-                <Grid item xs={3}>
-                    <Item>xs=3</Item>
-                </Grid>
-                <Grid item xs={3}>
-                    <Item>xs=3</Item>
-                </Grid>
-                <Grid item xs={3}>
-                    <Item>xs=3</Item>
-                </Grid>
-                <Grid item xs={3}>
-                    <Item>xs=3</Item>
-                </Grid>
-                <Grid item xs={3}>
-                    <Item>xs=3</Item>
-                </Grid>
-                <Grid item xs={3}>
-                    <Item>xs=3</Item>
-                </Grid>
-                <Grid item xs={3}>
-                    <Item>xs=3</Item>
-                </Grid>
-                <Grid item xs={3}>
-                    <Item>xs=3</Item>
-                </Grid>
-            </Grid>
-        </div>
+            </Grid >
+
+        </div >
     )
 };
 
-export default Home;
+export default Market;
