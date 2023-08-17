509d78cb1aa547941c911592373ffc1205900b2c
ThinhDang464 <thinhd464@gmail.com>
2023-08-17 17:34:18 +1000

    major updated dashboard
    update layout + mock data

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -3,7 +3,63 @@ import './Dashboard.css';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import { CardActionArea } from '@mui/material';
+import{
+    Chart as ChartJS,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+    Tooltip,
+    Legend
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+ChartJS.register(
+    BarElement,
+    CategoryScale,
+    LinearScale,
+    Tooltip,
+    Legend
+);
+const chartData = {
+    labels: ['January', 'February','March','April','May','June','July','August','September','October','November','December'],
+    datasets:[
+        {
+            label: 'Monetary assets gain',
+            backgroundColor: 'rgba(75,192,192,0.2)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(75,192,192,0.4)',
+            hoverBorderColor: 'rgba(75,192,192,1)',
+            data: [6500, 5900, 800, 810, 560,6000,70,800,90,100,20,3000]
+        }
+    ]
 
+};
+const chartOptions = {
+    layout: {
+        padding: {
+            bottom: 28 // Adjust this value as needed
+        }
+    },
+    scales: {
+        y: {
+            ticks: {
+                callback: function(value, index, values) {
+                    return '$' + value;
+                }
+            }
+        }
+    }
+};
+
+const mockHistoryData = [
+    { date: "2023-08-01", description: "Bought Apes", amount: "-$500" },
+    { date: "2023-08-02", description: "Sold Birds", amount: "+$2,000" },
+    { date: "2023-08-03", description: "Sold DracoNfts", amount: "+$5,000" },
+    { date: "2023-08-04", description: "Bought ChampsNFTs", amount: "-$200" },
+    { date: "2023-08-05", description: "Bought MomNFTs", amount: "-$150" },
+    { date: "2023-08-15", description: "Bought VitusNFTs", amount: "-$1000"},
+    { date: "2023-08-16", description: "Bought ValtusNFTs", amount: "-$1000"}
+];
 const Dashboard = () => {
     return (
         <div>
@@ -82,12 +138,32 @@ const Dashboard = () => {
                 </Grid>
                 {/* graph + history Grid container */}
                 <Grid container spacing={1} rowSpacing={1} item xs={12}>
-                    
+                    <Grid item xs={8}>
+                        <Card elevation={3} style={{ height: '400px'}}>
+                            <div style={{fontSize:'0.8rem',padding:'0.5rem'}}>Profile Overview</div>
+                            <div style={{height:'100%', display:'flex',justifyContent:'center'}}>
+                                <Bar data = {chartData} options={chartOptions}/>
+                            </div>
+                        </Card>
+                    </Grid>
+                    <Grid item xs = {4}>
+                        <Card elevation = {3} style={{height: '400px'}}>
+                            <div style={{fontSize:'0.8rem',padding:'0.5rem'}}>History</div>
+                            <div style={{overflowY:'auto',maxHeight:'350px'}}>
+                                {/* map iterates over each item,For each transaction in the array, 
+                                a new <div> element is created.*/}
+                                {mockHistoryData.map((transaction,index) =>(
+                                    <div key={index} style={{ padding: '10px', borderBottom: '1px solid #eee' }}>
+                                        <div><strong>Date:</strong> {transaction.date}</div>
+                                        <div><strong>Description:</strong> {transaction.description}</div>
+                                        <div><strong>Amount:</strong> {transaction.amount}</div>
+                                    </div>
+                                ))}
+                            </div>
+                        </Card>
+                    </Grid>
                 </Grid>
             </Grid>
-
-
-
 
         </div>
     )
