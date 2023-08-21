dde1911f68f961f653e657ca3a34282b608ee0ce
Kylzia <kylziabell@gmail.com>
2023-08-22 06:14:50 +1000

    Update with css

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,23 +1,36 @@
+import Grid from '@mui/material/Grid';
+import './about.css';
+
 const About = () => {
     return (
         <div>
             <h1>About</h1>
+            <Grid container spacing={2} rowSpacing={1} sx={{padding: '0 30px'}}>
+                    
+                <Grid item xs={12} md={8} rowSpacing={14}>
+                    
+                        <h2>Our Goals</h2>
+                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id ante sit amet mi rutrum tristique non fringilla tortor. In hac habitasse platea dictumst. </p>
 
-            <h2>Our Goals</h2>
+                        <h2>Who we are</h2>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id ante sit amet mi rutrum tristique non fringilla tortor. In hac habitasse platea dictumst. </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id ante sit amet mi rutrum tristique non fringilla tortor. In hac habitasse platea dictumst. </p>
+                    
+                </Grid>
 
-            <h2>Who we are</h2>
+                <Grid item xs={12} md={4}>
+                    
+                        <h2>What we do</h2>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id ante sit amet mi rutrum tristique non fringilla tortor. In hac habitasse platea dictumst. </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id ante sit amet mi rutrum tristique non fringilla tortor. </p>
 
-            <h2>What we do</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id ante sit amet mi rutrum tristique non fringilla tortor. </p>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id ante sit amet mi rutrum tristique non fringilla tortor. </p>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id ante sit amet mi rutrum tristique non fringilla tortor. </p>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id ante sit amet mi rutrum tristique non fringilla tortor. </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id ante sit amet mi rutrum tristique non fringilla tortor. </p>
+                    
+                </Grid>
+            </Grid>
         </div>
     )
 };
--- src/Pages/FAQ/FAQ.js
+++ src/Pages/FAQ/FAQ.js
@@ -1,13 +1,25 @@
 import { styled } from '@mui/material/styles';
+import { grey } from '@mui/material/colors';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
-import Card from '@mui/material/Card';
+
+import Typography from '@mui/material/Typography';
 
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
+
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import SearchIcon from '@mui/icons-material/Search';
+
+import Divider from '@mui/material/Divider';
+
+import InputBase from '@mui/material/InputBase';
+
+import './faq.css';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -22,53 +34,124 @@ const Item = styled(Paper)(({ theme }) => ({
 
 const FAQ = () => {
     const faqStuff = [
-        { accordMain: "General", accordSub: "banana" },
-        { accordMain: "Transaction", accordSub: "dog" },
-        { accordMain: "Security", accordSub: "fox" },
-        { accordMain: "Issues", accordSub: "hog" },
-        { accordMain: "Popular Questions", accordSub: "jil" },
-        { accordMain: "Useful Links", accordSub: "lik" },
+        { accordMain: "General", accordSub: [{ contentMain: "", contentSub: "" }] },
+        { accordMain: "Transaction", accordSub: [{ contentMain: "", contentSub: "" }] },
+        { accordMain: "Security", accordSub: [{ contentMain: "", contentSub: "" }] },
+        { accordMain: "Issues", accordSub: [{ contentMain: "", contentSub: "" }] },
+        { accordMain: "Popular Questions", accordSub: [{ contentMain: "", contentSub: "" }] },
+        { accordMain: "Useful Links", accordSub: [{ contentMain: "", contentSub: "" }] },
     ];
+
+    faqStuff[0].accordSub = [
+        { contentMain: "ball", contentSub: "giant" },
+        { contentMain: "cat", contentSub: "fox" },
+        { contentMain: "dog", contentSub: "eagle" },
+    ]
+    faqStuff[1].accordSub = [
+        { contentMain: "ball", contentSub: "giant" },
+        { contentMain: "cat", contentSub: "fox" },
+        { contentMain: "dog", contentSub: "eagle" },
+    ]
+    faqStuff[2].accordSub = [
+        { contentMain: "ball", contentSub: "giant" },
+        { contentMain: "cat", contentSub: "fox" },
+        { contentMain: "dog", contentSub: "eagle" },
+    ]
+    faqStuff[3].accordSub = [
+        { contentMain: "ball", contentSub: "giant" },
+        { contentMain: "cat", contentSub: "fox" },
+        { contentMain: "dog", contentSub: "eagle" },
+    ]
+    faqStuff[4].accordSub = [
+        { contentMain: "ball", contentSub: "giant" },
+        { contentMain: "cat", contentSub: "fox" },
+        { contentMain: "dog", contentSub: "eagle" },
+    ]
+    faqStuff[5].accordSub = [
+        { contentMain: "ball", contentSub: "giant" },
+        { contentMain: "cat", contentSub: "fox" },
+        { contentMain: "dog", contentSub: "eagle" },
+    ]
 
     return (
         <div>
             <h1>FAQ</h1>
 
-            <div>
-                <p>Search</p>
-            </div>
+            <Grid container direction="column" alignItems="center">
+                <TextField
+                    className='search-bar-faq'
+                    sx={{ color: grey[50] }}
+                    placeholder="Search..."
+                    InputProps={{
+                        className: 'yourWhiteness',
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <SearchIcon className='yourWhiteness'/>
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+            </Grid>
 
-            <Grid container spacing={1} rowSpacing={1}>
-                <Grid item xs={8}>
-                    <Item>
-                        <h2>Test</h2>
+            <Grid container direction="column" alignItems="center">
+                <div>
+                    <InputBase
+                        className= 'yourWhiteness'
+                        sx={{ ml: 1, flex: 1 }}
+                        placeholder="Search..."
+                        inputProps={{
+                        }}
+                    />
+                </div>
+            </Grid>
 
+            <Grid container spacing={2} rowSpacing={1} sx={{padding: '0 30px'}}>
+                <Grid item xs={12} md={8}>
+                    <div className='section3'>
                         {faqStuff.map((items, index) => (
-                            
-                            <>
-                                <Accordion>
-                                    <AccordionSummary
-                                    expandIcon={<ExpandMoreIcon />}
-                                    aria-controls="panel1a-content"
-                                    id="panel1a-header"
-                                    >
+                            <Accordion className='accordioning' TransitionProps={{ unmountOnExit: true }}>
+                                <AccordionSummary expandIcon={<ExpandMoreIcon className='yourWhiteness'/>} aria-controls="panel1a-content" id="panel1a-header">
                                     <Typography>{items.accordMain}</Typography>
-                                    </AccordionSummary>
-                                    <AccordionDetails>
-                                        <Typography>
-                                            {items.accordSub}
-                                        </Typography>
-                                    </AccordionDetails>
-                                </Accordion>
-                            </>
+                                </AccordionSummary>
+
+                                <AccordionDetails>
+                                    <Typography>
+                                        {items.accordSub.map((items2, index) => (
+                                            <Accordion className='accordioning' TransitionProps={{ unmountOnExit: true }}>
+                                                <AccordionSummary expandIcon={<ExpandMoreIcon className='yourWhiteness' />} aria-controls="panel1a-content" id="panel1a-header">
+                                                    <Typography>{items2.contentMain}</Typography>
+                                                </AccordionSummary>
+
+                                                <AccordionDetails>
+                                                    <Typography>
+                                                        {items2.contentSub}
+                                                    </Typography>
+                                                </AccordionDetails>
+                                            </Accordion>
+                                        ))}
+                                    </Typography>
+                                </AccordionDetails>
+                            </Accordion>
                         ))}
-                    </Item>
+                    </div>
                 </Grid>
 
-                <Grid item xs={4}>
-                    <Item>
-                        <h2>Test</h2>
-                    </Item>
+                <Grid item xs={12} md={4}>
+                    <div className='section3'>
+                        <Typography sx={{ m: 2 }} variant="h5">Need Help?</Typography>
+
+                        <Divider className='yourWhiteness' variant="middle" />
+
+                        <Typography sx={{ m: 4 }} variant="body1">+0000000000</Typography>
+
+                        <Typography sx={{ m: 4 }} variant="body1">xxx@xxx.xxx</Typography>
+
+                        <Typography sx={{ mt: 4 }} variant="body1">Weekdays</Typography>
+                        <Typography sx={{ mb: 4 }} variant="body1">9am-6pm</Typography>
+
+                        <Typography sx={{ mt: 4 }} variant="body1">Weekends</Typography>
+                        <Typography sx={{ mb: 2 }} variant="body1">10am-5pm</Typography>
+                    </div>
                 </Grid>
             </Grid>
         </div>
