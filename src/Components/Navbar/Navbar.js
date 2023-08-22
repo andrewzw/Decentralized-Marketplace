ba6e2c51285337ed1daf9bca48d2d36458ba3bce
andrewzw <yapzhewei2002@gmail.com>
2023-08-22 18:38:37 +1000

    updated styling

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <div className='container'>
             <NavLink to="/" className="left-container">
-                <img src="https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80" alt="Profile" className="logo-img" />
+                <img src="https://placehold.co/400" alt="Profile" className="logo-img" />
                 <div className='line'><h2>Safespace</h2></div>
             </NavLink>
 
