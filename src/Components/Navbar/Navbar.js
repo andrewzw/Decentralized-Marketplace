81c8639a7cec7281176421e41e32aa0ed0fc6d26
andrewzw <yapzhewei2002@gmail.com>
2023-08-22 18:26:41 +1000

    more styling

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,10 +3,13 @@ import './navbar.css';
 
 const Navbar = () => {
     return (
-        <>
+        <div className='container'>
+            <NavLink to="/" className="left-container">
+                <img src="https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80" alt="Profile" className="logo-img" />
+                <div className='line'><h2>Safespace</h2></div>
+            </NavLink>
 
-
-            <nav className="navbar-container">
+            <nav className="right-container">
 
                 <input type="text" placeholder="Search..." className="search-bar" />
                 <NavLink to="/" className="nav-button">Market</NavLink>
@@ -19,7 +22,7 @@ const Navbar = () => {
 
             </nav>
 
-        </>
+        </div>
     );
 };
 
