39f125a99ab7c5e60e6d17d1e12ba5d8f42e2cbc
andrewzw <yapzhewei2002@gmail.com>
2023-08-22 15:35:38 +1000

    navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,25 +1,23 @@
 import { NavLink } from 'react-router-dom';
 import './navbar.css';
+
 const Navbar = () => {
     return (
-        <>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink to="/">Market</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/dashboard">Dashboard</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/about">About Us</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/faq">FAQ</NavLink>
-                    </li>
-                </ul>
+        <div>
+            <nav className="navbar-container">
+                <div className='left'>
+                    <input type="text" placeholder="Search..." className="search-bar" />
+                    <NavLink to="/" className="nav-button">Market</NavLink>
+                    <NavLink to="/about" className="nav-button">About Us</NavLink>
+                    <NavLink to="/faq" className="nav-button">FAQ</NavLink>
+                </div>
+
+                <NavLink to="/dashboard" className="profile-button">
+                    <img src="https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80" alt="Profile" className="profile-img" />
+                </NavLink>
+
             </nav>
-        </>
+        </div>
     );
 };
 
