b591a077b329afbce9e5bcfc133ca4dd5112c0ec
andrewzw <yapzhewei2002@gmail.com>
2023-08-23 14:00:44 +1000

    removed styled import as it's no longer used

--- src/Pages/Market/Market.js
+++ src/Pages/Market/Market.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
