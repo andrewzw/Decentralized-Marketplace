86a23b8789f351d346aa81621029e717c4bb685e
Kylzia <kylziabell@gmail.com>
2023-08-24 20:47:50 +1000

    about page update

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,21 +3,22 @@ import CardMedia from '@mui/material/CardMedia';
 import Divider from '@mui/material/Divider';
 
 import { grey } from '@mui/material/colors';
-import SellIcon from '@mui/icons-material/Sell';
-import TrendingUpIcon from '@mui/icons-material/TrendingUp';
-import PaletteIcon from '@mui/icons-material/Palette';
-import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
 
 import './about.css';
+import goalImage1 from '../../Assets/goals/trend.png';
+import goalImage2 from '../../Assets/goals/tag.png';
+import goalImage3 from '../../Assets/goals/palette.png';
+import goalImage4 from '../../Assets/goals/support.png';
+
 import memberImage1 from '../../Assets/members/Tom.jpeg';
 import memberImage2 from '../../Assets/members/Andrew.JPG';
 import memberImage3 from '../../Assets/members/Yuan.jpg';
 
 const goalImage = [
-    { imaging: memberImage1, alting: "Tom" },
-    { imaging: memberImage2, alting: "Yuan" },
-    { imaging: memberImage3, alting: "Andrew" },
-    { imaging: memberImage3, alting: "Andrew" },
+    { imaging: goalImage1, alting: "trend", words: "See the rise of your NFT" },
+    { imaging: goalImage2, alting: "tag", words: "Create and Sell your NFT" },
+    { imaging: goalImage3, alting: "palette", words: "Browse our various NFTs" },
+    { imaging: goalImage4, alting: "support", words: "Support the NFT creators" },
 ]
 
 const peopleImage = [
@@ -30,7 +31,7 @@ const About = () => {
     return (
         <div>
             <h1>About</h1>
-            <Grid container spacing={2} rowSpacing={1} sx={{padding: '0 30px'}}>
+            <Grid container spacing={2} rowSpacing={1} className='about-padding'>
                     
                 <Grid item xs={12} md={8} rowSpacing={1}>
                     
@@ -42,35 +43,19 @@ const About = () => {
                             We want to revolutionise how people see, value, and engage with digital art and collectibles.
                         </p>
 
-                        <Grid container spacing={0.5} rowSpacing={0.5} >
-                            <Grid item xs={6} md={3}>
-                                <TrendingUpIcon className='yourWhiteness' sx={{
-                                        minHeight: "100px",
-                                        minWidth: "100px",
-                                }}/>
-                                <span>See the rise of your NFT</span>
-                            </Grid>
-                            <Grid item xs={6} md={3}>
-                                <SellIcon className='yourWhiteness' sx={{
-                                        minHeight: "100px",
-                                        minWidth: "100px",
-                                }}/>
-                                <span>Create and Sell your NFT</span>
-                            </Grid>
-                            <Grid item xs={6} md={3}>
-                                <PaletteIcon className='yourWhiteness' sx={{
-                                        minHeight: "100px",
-                                        minWidth: "100px",
-                                }}/>
-                                <span>Browse our various NFTs</span>
-                            </Grid>
-                            <Grid item xs={6} md={3}>
-                                <VolunteerActivismIcon className='yourWhiteness' sx={{
-                                        minHeight: "100px",
-                                        minWidth: "100px",
-                                }}/>
-                                <span>Support the NFT creators</span>
-                            </Grid>
+                        <Grid container spacing={0.5} rowSpacing={0.5}>
+                            {goalImage.map((items, index) => (
+                                <Grid item xs={6} md={3}>
+                                    <CardMedia
+                                        component="img"
+                                        image={items.imaging}
+                                        alt={items.alting}
+                                        className='about-goal-image'
+                                    />
+
+                                    <p className='about-goal-text'>{items.words}</p>
+                                </Grid>
+                            ))}
                         </Grid>
 
                         <h2>Who we are</h2>
@@ -88,39 +73,37 @@ const About = () => {
                                         width="100%"
                                         image={items.imaging}
                                         alt={items.alting}
-                                        sx={{
-                                            borderRadius: '10px',
-                                            maxHeight: "300px",
-                                            minHeight: "50px",
-                                            maxWidth: "300px",
-                                            minWidth: "50px",
-                                        }}
+                                        className='about-member-image'
                                     />
                                 </Grid>
                             ))}
                         </Grid>
                 </Grid>
 
-                
-
                 <Grid item xs={12} md={4}>
-                    <div className='cards2'>
+                    <div className='about-aside'>
                         <h2>What we offer</h2>
 
                         <p>
-                        Discover Unique masterpieces: If you're looking for unique digital masterpieces, look no further than our marketplace.
+                            Discover Unique masterpieces: If you're looking for unique digital masterpieces, look no further than our marketplace.
                         </p>
+
                         <Divider className='yourWhiteness' sx={{borderColor: grey[50]}} variant="middle" />
+
                         <p>
-                        Empowerment through Ownership: By purchasing an NFT, you get a digital product and you can support your favoured artists on their journey.
+                            Empowerment through Ownership: By purchasing an NFT, you get a digital product and you can support your favoured artists on their journey.
                         </p>
+
                         <Divider className='yourWhiteness' sx={{borderColor: grey[50]}} variant="middle" />
+
                         <p>
-                        Transparent and Secure Transactions: Our system is based on blockchain technology which guarantees the privacy of your financial transactions and personal data.
+                            Transparent and Secure Transactions: Our system is based on blockchain technology which guarantees the privacy of your financial transactions and personal data.
                         </p>
+
                         <Divider className='yourWhiteness' sx={{borderColor: grey[50]}} variant="middle" />
+
                         <p>
-                        Community and Collaboration: "SafeSpace" has an active group of artists, collectors, and fans, it is more than just a marketplace. 
+                            Community and Collaboration: "SafeSpace" has an active group of artists, collectors, and fans, it is more than just a marketplace. 
                         </p>
                     </div>
                     
