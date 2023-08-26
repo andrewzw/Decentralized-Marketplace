a5b8fe817696928564da11cab1558c5a01943916
andrewzw <yapzhewei2002@gmail.com>
2023-08-26 23:44:47 +1000

    clean up code and styled login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,29 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+import VpnKeyIcon from "@mui/icons-material/VpnKey";
+import "./login.css";
 
 function Login() {
   const navigate = useNavigate();
-  const [wallet, setWallet] = useState(''); // assuming a wallet string for prototype
+  const [wallet, setWallet] = useState(""); // assuming a wallet string for prototype
 
   function handleLogin() {
-    if(wallet) {  // Just a simple check to see if something was entered
-      localStorage.setItem('isLoggedIn', 'true');
+    if (wallet) {
+      // Just a simple check to see if something was entered
+      localStorage.setItem("isLoggedIn", "true");
       navigate("/Dashboard");
     } else {
-      alert('Please enter your wallet info (it can be anything for this prototype)');
+      alert(
+        "Please enter your wallet info (it can be anything for this prototype)"
+      );
     }
   }
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '45vh' }}>
-      <input 
-        type="text" 
-        placeholder="Enter wallet to sign in..." 
-        value={wallet} 
-        onChange={(e) => setWallet(e.target.value)} 
-        style={{ padding: '10px', marginBottom: '20px', fontSize: '16px', width: '250px' }}
-      />
-      <button onClick={handleLogin} style={{ padding: '10px 20px', fontSize: '16px' }}>Login</button>
+    <div className="login">
+      <h1>Dashboard Login</h1>
+
+      <div className="login-box">
+        <p>Enter your login details:</p>
+        <input
+          type="text"
+          placeholder="Enter wallet to sign in..."
+          value={wallet}
+          onChange={(e) => setWallet(e.target.value)}
+          className="login-input"
+        />
+        <Button
+          onClick={handleLogin}
+          className="login-button"
+          variant="contained"
+          endIcon={<VpnKeyIcon />}
+        >
+          Login
+        </Button>
+      </div>
     </div>
   );
 }
